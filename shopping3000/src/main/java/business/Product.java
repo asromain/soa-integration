681d67a74bb82309213da4c9b78ffa3eb372302c
Christensen Pierre <snoopwarrior@hotmail.fr>
2015-10-26 17:04:27 +0100

    Ajout classe Product

---
+++ shopping3000/src/main/java/business/Product.java
@@ -0,0 +1,33 @@
+package business;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Pierre on 26/10/2015.
+ *
+ * Represente l'objet metier Produit des notre Mall
+ *
+ * specializedAttributes -> proprietes specifiques aux differentes boutiques
+ */
+public class Product {
+
+    private String name;
+    private String idShop;
+    private String cost;
+
+    private Map<String, String> specializedAttributes;
+
+    public Product(String attributs) {
+        JSONObject product = new JSONObject(attributs);
+        this.specializedAttributes = new HashMap<String, String>();
+
+       
+
+
+
+    }
+
+}
