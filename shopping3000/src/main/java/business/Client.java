c94126149c4c2395c852f5ed724ea912ab28296b
maxdestors <max.destors@gmail.com>
2015-10-29 21:35:47 +0100

    client class

--- shopping3000/src/main/java/business/Client.java
+++ shopping3000/src/main/java/business/Client.java
@@ -1,11 +1,64 @@
 package business;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Created by Florian Rouyer on 26/10/2015.
+ * Represente l'objet metier Client de notre Mall
+ *
+ *
  */
 public class Client implements Serializable {
-    public Client() {
+
+    private String id;
+    private String name;
+    private String address;
+
+
+    private Map<String, String> specializedAttributes;
+
+    public Client(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.specializedAttributes = new HashMap<String, String>();
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSpecializedAttribute(String key) {
+        return specializedAttributes.get(key);
+    }
+
+    public void setSpecializedAttribute(String key, String value) {
+        this.specializedAttributes.put(key, value);
+    }
+
+    public String deleteSpecializedAttribute(String key) {
+        return specializedAttributes.remove(key);
+    }
+
 }
